5f4be520bf317880d04328c513b60c7a257f3907
Tran Van Cong <aanh7108@gmail.com>
2024-05-31 22:08:16 +0700

    create project01

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Trang chu");
+  })
+
+
+
+app.get('/product',(req, res) => {
+    res.send("Trang san pham");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`hello ${port}`)
+})
