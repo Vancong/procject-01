4e270782fb63d34b1900984755fcf08ef22ae053
Tran Van Cong <aanh7108@gmail.com>
2024-06-08 18:37:43 +0700

    lay data san pham in ra giao dien

--- controllers/admin/products.controllers.js
+++ controllers/admin/products.controllers.js
@@ -1,5 +1,12 @@
-module.exports.index= (req, res) => {
+const Prodcut=require('../../models/product.models.js');
+
+module.exports.index= async (req, res) => {
+    const products= await Prodcut.find({
+      deleted: false
+    });
+    console.log(products);
     res.render('admin/page/products/index.pug', {
-      pageTitle: "Quan ly san pham"
+      pageTitle: "Quan ly san pham",
+      products: products
     });
   };
