801dc403c3ac44be636f61650381f7b674374643
Tran Van Cong <aanh7108@gmail.com>
2024-06-08 20:47:38 +0700

    tinh nang loc theo trang thai

--- controllers/admin/products.controllers.js
+++ controllers/admin/products.controllers.js
@@ -1,10 +1,20 @@
 const Prodcut=require('../../models/product.models.js');
 
 module.exports.index= async (req, res) => {
-    const products= await Prodcut.find({
+    const find= {
       deleted: false
-    });
-    console.log(products);
+    };
+
+    if(req.query.status){
+      find.status=req.query.status;
+    }
+
+    const products= await Prodcut.find(find);
+
+    // console.log(products);
+
+
+
     res.render('admin/page/products/index.pug', {
       pageTitle: "Quan ly san pham",
       products: products
