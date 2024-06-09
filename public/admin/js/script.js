8d59885875384cdaf4d9a65079792266d3acd906
Tran Van Cong <aanh7108@gmail.com>
2024-06-09 22:29:49 +0700

    lam chuc nang bo loc

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,26 @@
-console.log("ok");
+// button status
+const listButton=document.querySelectorAll("[button-status");
+if (listButton.length>0) {
+    const url= new URL(window.location.href); // tao ra link moi
+   listButton.forEach(button => {
+    // console.log(url.href);
+        button.addEventListener("click", () => {
+            const status=button.getAttribute("button-status");
+            // console.log(status);
+            if (status) {
+                url.searchParams.set("status",status);
+            }
+            else {
+                url.searchParams.delete("status");
+            }
+            window.location.href=url.href;  // chuyen sang trang moi
+        })
+   });
+    const statusCr=url.searchParams.get("status")|| "";
+    const buttonCr=document.querySelector(`[button-status="${statusCr}"]`);
+    // console.log(buttonCr);
+   if (buttonCr) {
+    buttonCr.classList.add("active");
+   }
+}
+
