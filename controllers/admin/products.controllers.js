36bcdb7745b0ead042238874e0a7d48715fbd254
Tran Van Cong <aanh7108@gmail.com>
2024-06-11 15:35:34 +0700

    tinh nang tim kiem backend

--- controllers/admin/products.controllers.js
+++ controllers/admin/products.controllers.js
@@ -5,18 +5,30 @@ module.exports.index= async (req, res) => {
       deleted: false
     };
 
+    // bo loc
     if(req.query.status){
       find.status=req.query.status;
     }
 
+    // tim kiem
+    let keyword="";
+
+    if (req.query.keyword){
+        const regax= new RegExp(req.query.keyword,"i");  //tim kiem tuong doi
+        find.title=regax;
+        keyword=req.query.keyword;
+    }
+
+    //end tim kiem
+
+    
+
     const products= await Prodcut.find(find);
-
     // console.log(products);
-
-
 
     res.render('admin/page/products/index.pug', {
       pageTitle: "Quan ly san pham",
-      products: products
+      products: products,
+      keyword: keyword
     });
   };
